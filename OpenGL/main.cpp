94003b73ba520596f4e7cd8adb81b84bf5c214a2
mohankri <mohankri@gmail.com>
2017-08-21 22:17:33 -0700

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Krishna Mohan on 8/21/17.
+//  Copyright © 2017 Krishna Mohan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
